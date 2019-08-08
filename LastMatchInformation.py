8f674bd33daa51f00067994cd1b0f483c87752bc
anhuynhh <anhuynhh@gmail.com>
2019-08-08 19:30:32 +0200

    Object to hold presentation information

---
+++ LastMatchInformation.py
@@ -0,0 +1,9 @@
+class LastMatchInformation:
+    def __init__(self, match_id, kills, time, role, win, lane, hero):
+        self.match_id = match_id
+        self.kills = kills
+        self.time = time
+        self.role = role
+        self.win = win
+        self.lane = lane
+        self.hero = hero
